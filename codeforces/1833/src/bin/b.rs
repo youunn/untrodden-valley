e2fe62784fdbb42f57e0c5ace97630dcf1cd04f9
houjuunue <i@nue.im>
2023-05-25 01:05:36 +0800

    add 1833

---
+++ codeforces/1833/src/bin/b.rs
@@ -0,0 +1,126 @@
+type Void = Result<(), Box<dyn std::error::Error>>;
+
+fn solve(io: &mut io::IO) -> Void {
+    let (n, _k) = io.read2::<usize, u64>()?;
+    let mut a = io.read_veci::<i64, Vec<_>>(n)?;
+    let mut b = io.read_vec::<i64, Vec<_>>(n)?;
+    a.sort_by_key(|(_, v)| *v);
+    b.sort();
+    let mut c = a.into_iter().zip(b.into_iter()).collect::<Vec<_>>();
+    c.sort_by_key(|((i, _), _)| *i);
+    io.print_vec(c.into_iter().map(|c| c.1))?;
+    Ok(())
+}
+
+fn main() -> Void {
+    let mut io = io::IO::new();
+    for _ in 0..io.read::<usize>()? {
+        solve(&mut io)?;
+    }
+    Ok(())
+}
+
+#[allow(dead_code)]
+mod io {
+    use std::{
+        error::Error,
+        fmt::Display,
+        io::{stdin, stdout, BufRead, BufWriter, StdinLock, StdoutLock, Write},
+        str::FromStr,
+    };
+
+    macro_rules! read_impl {
+        ( $f:ident, $($t:ident),+ ) => {
+            #[allow(unused_parens)]
+            pub fn $f<$($t),+>(&mut self) -> Result<($($t),+), Box<dyn std::error::Error>>
+            where
+                $($t: std::str::FromStr,
+                <$t>::Err: std::error::Error + 'static,)+
+            {
+                let mut s: String = String::new();
+                self.scan.read_line(&mut s)?;
+                let mut s = s.split_whitespace();
+                Ok(($(s.next().ok_or("")?.parse::<$t>()?),+))
+            }
+        };
+    }
+
+    pub struct IO<'a> {
+        scan: StdinLock<'a>,
+        out: BufWriter<StdoutLock<'a>>,
+    }
+
+    impl<'a> IO<'a> {
+        pub fn new() -> Self {
+            Self {
+                scan: stdin().lock(),
+                out: BufWriter::new(stdout().lock()),
+            }
+        }
+
+        read_impl!(read, T1);
+        read_impl!(read2, T1, T2);
+        read_impl!(read3, T1, T2, T3);
+
+        pub fn read_line(&mut self) -> Result<String, Box<dyn Error>> {
+            let mut s: String = String::new();
+            self.scan.read_line(&mut s)?;
+            Ok(s)
+        }
+
+        pub fn read_vec<T, V>(&mut self, n: usize) -> Result<V, Box<dyn Error>>
+        where
+            T: FromStr,
+            <T>::Err: Error + 'static,
+            V: FromIterator<T>,
+        {
+            let mut s: String = String::new();
+            self.scan.read_line(&mut s)?;
+            let v = s
+                .split_whitespace()
+                .take(n)
+                .map(&str::parse)
+                .collect::<Result<V, _>>()?;
+            Ok(v)
+        }
+
+        pub fn read_veci<T, V>(&mut self, n: usize) -> Result<V, Box<dyn Error>>
+        where
+            T: FromStr,
+            <T>::Err: Error + 'static,
+            V: FromIterator<(usize, T)>,
+        {
+            let mut s: String = String::new();
+            self.scan.read_line(&mut s)?;
+            let v = s
+                .split_whitespace()
+                .take(n)
+                .enumerate()
+                .map(|(i, v)|match v.parse() {
+                    Ok(v) => Ok((i, v)),
+                    Err(e) => Err(e),
+                })
+                .collect::<Result<V, _>>()?;
+            Ok(v)
+        }
+
+        pub fn print<T: Display>(&mut self, value: T) -> Result<(), Box<dyn Error>> {
+            writeln!(self.out, "{}", value)?;
+            Ok(())
+        }
+
+        pub fn print_vec<T: Display>(
+            &mut self,
+            mut values: impl Iterator<Item = T>,
+        ) -> Result<(), Box<dyn Error>> {
+            if let Some(v) = values.next() {
+                write!(self.out, "{}", v)?;
+                for v in values {
+                    write!(self.out, " {}", v)?;
+                }
+            }
+            writeln!(self.out)?;
+            Ok(())
+        }
+    }
+}
