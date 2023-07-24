bc121974584f7998e3a0375087878cac9578f31f
houjuunue <i@nue.im>
2023-07-24 22:41:06 +0800

    format files in cp lib

--- cp/src/cmp.rs
+++ cp/src/cmp.rs
@@ -5,16 +5,16 @@ pub struct max;
 #[allow(non_camel_case_types)]
 pub struct max_partial<T: Ord>(T);
 impl<T: Ord> Div<T> for max_partial<T> {
-    type Output = T;
-    fn div(self, rhs: T) -> Self::Output {
-        self.0.max(rhs)
-    }
+	type Output = T;
+	fn div(self, rhs: T) -> Self::Output {
+		self.0.max(rhs)
+	}
 }
 impl<T: Ord> Div<T> for max {
-    type Output = max_partial<T>;
-    fn div(self, rhs: T) -> Self::Output {
-        max_partial(rhs)
-    }
+	type Output = max_partial<T>;
+	fn div(self, rhs: T) -> Self::Output {
+		max_partial(rhs)
+	}
 }
 
 #[allow(non_camel_case_types)]
@@ -22,46 +22,46 @@ pub struct min;
 #[allow(non_camel_case_types)]
 pub struct min_partial<T: Ord>(T);
 impl<T: Ord> Div<T> for min_partial<T> {
-    type Output = T;
-    fn div(self, rhs: T) -> Self::Output {
-        self.0.min(rhs)
-    }
+	type Output = T;
+	fn div(self, rhs: T) -> Self::Output {
+		self.0.min(rhs)
+	}
 }
 impl<T: Ord> Div<T> for min {
-    type Output = min_partial<T>;
-    fn div(self, rhs: T) -> Self::Output {
-        min_partial(rhs)
-    }
+	type Output = min_partial<T>;
+	fn div(self, rhs: T) -> Self::Output {
+		min_partial(rhs)
+	}
 }
 
 macro_rules! cmp_impl {
-    ($t:ty) => {
-        impl Div<max> for $t {
-            type Output = max_partial<$t>;
-            fn div(self, _: max) -> Self::Output {
-                max_partial(self)
-            }
-        }
-        impl DivAssign<max_partial<$t>> for $t {
-            fn div_assign(&mut self, rhs: max_partial<$t>) {
-                *self = (*self).max(rhs.0)
-            }
-        }
-        impl Div<min> for $t {
-            type Output = min_partial<$t>;
-            fn div(self, _: min) -> Self::Output {
-                min_partial(self)
-            }
-        }
-        impl DivAssign<min_partial<$t>> for $t {
-            fn div_assign(&mut self, rhs: min_partial<$t>) {
-                *self = (*self).min(rhs.0)
-            }
-        }
-    };
-    ($($t:ty),+) => {
-        $(cmp_impl!($t);)+
-    };
+	($t:ty) => {
+		impl Div<max> for $t {
+			type Output = max_partial<$t>;
+			fn div(self, _: max) -> Self::Output {
+				max_partial(self)
+			}
+		}
+		impl DivAssign<max_partial<$t>> for $t {
+			fn div_assign(&mut self, rhs: max_partial<$t>) {
+				*self = (*self).max(rhs.0)
+			}
+		}
+		impl Div<min> for $t {
+			type Output = min_partial<$t>;
+			fn div(self, _: min) -> Self::Output {
+				min_partial(self)
+			}
+		}
+		impl DivAssign<min_partial<$t>> for $t {
+			fn div_assign(&mut self, rhs: min_partial<$t>) {
+				*self = (*self).min(rhs.0)
+			}
+		}
+	};
+	($($t:ty),+) => {
+		$(cmp_impl!($t);)+
+	};
 }
 
 cmp_impl!(u8, u16, u32, u64, u128, usize);
