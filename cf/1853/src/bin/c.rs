4c4ab7152d3eb760db376422190781a6c2b6a6b0
houjuunue <i@nue.im>
2023-07-26 18:08:53 +0800

    add 1853 c

--- cf/1853/src/bin/c.rs
+++ cf/1853/src/bin/c.rs
@@ -2,10 +2,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let mut io = cp::cio::default();
 	let t: usize = io.get()?;
 	for _ in 0..t {
-		let n: usize = io.get()?;
-		let v: Vec<usize> = io.get_vec(n)?;
+		let n = io.get::<usize>()?;
+		let k = io.get::<usize>()?;
+		let v = io.get_vec::<usize, Vec<_>>(n)?;
 
-		io.put(v[0])?;
+		let mut ans = 1;
+		if v[0] > 1 {
+			io.put(ans)?;
+			continue;
+		}
+		
+		let mut i = 0;
+		for _ in 0..k {
+			ans += i as u64;
+			while i < n && v[i] as u64 <= ans {
+				i += 1;
+				ans += 1;
+			}
+		}
+
+		io.put(ans)?;
 	}
 
 	Ok(())
