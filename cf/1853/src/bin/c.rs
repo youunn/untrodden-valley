a7a4e5b0c8c43e465f5f65177ca2e98244ac1345
houjuunue <i@nue.im>
2023-07-26 14:33:16 +0800

    try pin and remove pin

--- cf/1853/src/bin/c.rs
+++ cf/1853/src/bin/c.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let mut io = cp::io_test::IO::default();
+	let mut io = cp::io_test::default();
 	let t: usize = io.get()?;
 	let n: usize = io.get()?;
 	io.put(t + n)?;
--- cp/src/io_test.rs
+++ cp/src/io_test.rs
@@ -2,21 +2,62 @@ use std::{
 	error::Error,
 	fmt::Display,
 	io::{stdin, stdout, BufRead, BufWriter, StdinLock, StdoutLock, Write},
-	str::FromStr,
+	ptr::NonNull,
+	str::{from_utf8_unchecked, FromStr, SplitWhitespace},
 };
 
-pub struct GeneralIO<I, O> {
-	scan: I,
-	out: O,
-	buf: String,
+pub struct Buf<'a> {
+	// never mutate the buffer directly
+	buf: Vec<u8>,
+	iter: SplitWhitespace<'a>,
 }
 
-impl<I: BufRead, O: Write> GeneralIO<I, O> {
+impl<'a> Buf<'a> {
+	pub fn new() -> Self {
+		Self::default()
+	}
+
+	pub fn modify<F>(&mut self, f: F) -> Result<(), Box<dyn Error>>
+	where
+		F: FnOnce(&mut Vec<u8>) -> Result<(), Box<dyn Error>>,
+	{
+		f(&mut self.buf)?;
+		let ptr = NonNull::from(&self.buf);
+		let buf = unsafe { from_utf8_unchecked(ptr.as_ref()) };
+		self.iter = buf.split_whitespace();
+		Ok(())
+	}
+}
+
+impl<'a> Default for Buf<'a> {
+	fn default() -> Self {
+		Self {
+			buf: Default::default(),
+			iter: "".split_whitespace(),
+		}
+	}
+}
+
+impl<'a> Iterator for Buf<'a> {
+	type Item = &'a str;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.iter.next()
+	}
+}
+
+pub struct GeneralIO<'a, I, O> {
+	scan: I,
+	out: O,
+	buf: Buf<'a>,
+}
+
+impl<I: BufRead, O: Write> GeneralIO<'_, I, O> {
 	fn new(scan: I, out: O) -> Self {
 		Self {
 			scan,
 			out,
-			buf: String::new(),
+			buf: Buf::new(),
 		}
 	}
 
@@ -26,11 +67,14 @@ impl<I: BufRead, O: Write> GeneralIO<I, O> {
 		<T as FromStr>::Err: Error + 'static,
 	{
 		loop {
-			if let Some(s) = self.buf.split_whitespace().next() {
+			if let Some(s) = self.buf.next() {
 				return Ok(s.parse()?);
 			}
-			self.buf.clear();
-			self.scan.read_line(&mut self.buf)?;
+			self.buf.modify(|buf| {
+				buf.clear();
+				self.scan.read_until(b'\n', buf)?;
+				Ok(())
+			})?;
 		}
 	}
 
@@ -40,10 +84,14 @@ impl<I: BufRead, O: Write> GeneralIO<I, O> {
 	}
 }
 
-pub type IO<'a> = GeneralIO<StdinLock<'a>, BufWriter<StdoutLock<'a>>>;
+type IO<'a> = GeneralIO<'a, StdinLock<'a>, BufWriter<StdoutLock<'a>>>;
 
 impl Default for IO<'_> {
 	fn default() -> Self {
 		Self::new(stdin().lock(), BufWriter::new(stdout().lock()))
 	}
+}
+
+pub fn default<'a>() -> IO<'a> {
+	IO::default()
 }
